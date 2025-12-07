1d603839d7a96518d34bc317e93f13cbf454a706
sanket-forge <sanketkhatua9861@gmail.com>
2025-12-07 18:26:56 +0530

    connect to mongo

--- src/index.js
+++ src/index.js
@@ -1,11 +1,44 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+// require('dotenv').config({path: './.env'})
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({ path: "./.env" });
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+import express from "express"
+const app = express()
 
 async function DBConnection() {
     
     try {
         
        await mongoose.connect(`${process.env.MONGODB_URL} / ${DB_NAME}`)
+
+       app.on("error", (error) => {
+
+        console.log("Error: ", error)
+       })
+
+       app.listen(process.env.PORT, () => {
+
+        console.log(`App is listening on port ${process.env.PORT}`)
+       })
+
     } catch (error) {
         
         console.error("DB connection failed: ", error)
@@ -13,3 +46,5 @@ async function DBConnection() {
 }
 
 DBConnection()
+
+*/
